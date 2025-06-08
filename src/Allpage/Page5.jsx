a847fd9e9a2d41530a77ea0c53706210a86ccb0d
Oshirigami <104879528+BANK12384@users.noreply.github.com>
2025-06-08 15:45:54 +0700

    Page 5

--- src/Allpage/Page5.jsx
+++ src/Allpage/Page5.jsx
@@ -1,10 +1,58 @@
 import { useState } from "react";
 import "../App.css";
 
-export default function Page5 (){
-  return(
-    <section className="w-screen h-screen" id="classroom">
+const slides = [{ imageUrl: "/img/Page41.svg" },
+  {imageUrl: "/img/Page42.svg"},
+  {imageUrl: "/img/Page43.svg"},
+  {imageUrl: "/img/Page44.svg"},
+  {imageUrl: "/img/Page45.svg"},
+];
 
+function SlidePage5({ imageUrl }) {
+  return (
+    <div
+      className="top-0 left-0 w-full h-full bg-center bg-cover bg-fixed z-0"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+  );
+}
+
+export default function Page5() {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const hasMultipleSlides = slides.length > 1;
+
+  const nextSlide = () => setSlideIndex((prev) => (prev + 1) % slides.length);
+  const prevSlide = () =>
+    setSlideIndex((prev) => (prev - 1 + slides.length) % slides.length);
+
+  return (
+    <section className="w-screen h-screen relative overflow-hidden" id="classroom">
+      <div className="w-full h-[15%] bg-white flex flex-col justify-center items-center shadow-lg">
+        <h1 className="text-4xl Thai">หลักสูตรที่เปิดสอน</h1>
+        <div className="bg-black w-[18%] h-[0.35rem] rounded-full mt-2" />
+      </div>
+      {/* Background image */}
+      <SlidePage5 imageUrl={slides[slideIndex].imageUrl} className="z-10"/>
+
+      {/* Navigation Buttons */}
+      {hasMultipleSlides && (
+        <>
+          <button
+            onClick={prevSlide}
+            aria-label="Previous Slide"
+            className="absolute z-40 left-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full RotateboxLeft"
+          >
+            <img src="/Left-arrow.svg" alt="←" className="w-10" />
+          </button>
+          <button
+            onClick={nextSlide}
+            aria-label="Next Slide"
+            className="absolute z-40 right-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full RotateboxRight"
+          >
+            <img src="/right-arrow.svg" alt="→" className="w-10" />
+          </button>
+        </>
+      )}
     </section>
   );
 }
--- src/Allpage/Page6.jsx
+++ src/Allpage/Page6.jsx
@@ -3,7 +3,108 @@ import "../App.css";
 
 export default function Page5() {
   const [count, setCount] = useState(0);
-  return <section className="w-screen h-screen bg-black" id="page6">
+  return (
+  <section className="w-screen h-screen bg-[#F2CF67]" id="page6">
+    {/* Background image */}
+    <div className="absolute grid grid-cols-2 w-full h-full">
+      <div className=" absolute grid mt-[41%] justify-end items-end w-full pr-4">
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+      </div>
+
+      <div className=" absolute grid mt-10 justify-start items-start w-full pl-4">
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+        <div className="grid grid-cols-7 mb-5">
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+          <div className="w-2 h-2 bg-black rounded-full mx-3"></div>
+        </div>
+      </div>
+    </div>
     
-  </section>;
+    
+    
+  </section>);
 }
