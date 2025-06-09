797e5873e0e63f9981584c8d2efdb98c83350bbf
Oshirigami <104879528+BANK12384@users.noreply.github.com>
2025-06-09 13:43:42 +0700

    Decktop finish

--- src/Allpage/Page1.jsx
+++ src/Allpage/Page1.jsx
@@ -3,20 +3,45 @@ import "../App.css";
 
 export default function Page1() {
   const [count, setCount] = useState(0);
-  
+
   return (
-    <section className=" w-screen h-screen grid content-center z-20 text-center justify-center justify-items-center" id="home">
-      <div class="absolute inset-0 bg-[url('/img/BG-Hone.png')] bg-cover bg-center bg-fixed h-full z-10"></div>
-      <div class="absolute inset-0 bg-black bg-opacity-75 z-10"></div>
-      <h1 className="Eng  text-6xl font-bold text-white z-20 my-5 leading-[4.5rem]">
-        Welcome to Gina
-        <br /> <span className="text-[#1D6FE1]">Eng</span>
-        <span className="text-[#FF0004]">lish</span> Center
-      </h1>
-      <div className="w-[100%] h-1 bg-white z-20  self-center rounded-full"></div>
-      <p className="Thai z-20 my-4 text-4xl text-white">
-        ติวเตอร์ภาษาอังกฤษที่ตอบโจทย์ทุกไลฟ์สไตล์!
-      </p>
+    <section
+      className=" w-screen h-screen"
+      id="home"
+    >
+      {/* ------------------------------------------------------------------------------------------------------------------------------- */}
+      {/* Decktop */}
+      <div className="w-screen h-screen lg:grid content-center z-20 text-center justify-center justify-items-center hidden">
+        <div class="absolute inset-0 bg-[url('/img/BG-Hone.png')] bg-cover bg-center bg-fixed h-full z-10"></div>
+        <div class="absolute inset-0 bg-black bg-opacity-75 z-10"></div>
+        <h1 className="Eng  text-6xl font-bold text-white z-20 my-5 leading-[4.5rem]">
+          Welcome to Gina
+          <br /> <span className="text-[#1D6FE1]">Eng</span>
+          <span className="text-[#FF0004]">lish</span> Center
+        </h1>
+        <div className="w-[100%] h-1 bg-white z-20  self-center rounded-full"></div>
+        <p className="Thai z-20 my-4 text-4xl text-white">
+          ติวเตอร์ภาษาอังกฤษที่ตอบโจทย์ทุกไลฟ์สไตล์!
+        </p>
+      </div>
+      {/* ------------------------------------------------------------------------------------------------------------------------------- */}
+
+      {/* ------------------------------------------------------------------------------------------------------------------------------- */}
+      {/* Moblie */}
+      <div className="w-screen h-screen grid content-center z-20 text-center justify-center justify-items-center lg:hidden">
+        <div class="absolute inset-0 bg-[url('/img/BG-Hone.png')] bg-cover bg-center bg-fixed h-full z-10"></div>
+        <div class="absolute inset-0 bg-black bg-opacity-75 z-10"></div>
+        <h1 className="Eng text-4xl font-bold text-white z-20 my-4 leading-[2.5rem]">
+          Welcome to Gina
+          <br /> <span className="text-[#1D6FE1]">Eng</span>
+          <span className="text-[#FF0004]">lish</span> Center
+        </h1>
+        <div className="w-[100%] h-1 bg-white z-20  self-center rounded-full"></div>
+        <p className="Thai z-20 my-2 text-lg text-white">
+          ติวเตอร์ภาษาอังกฤษที่ตอบโจทย์ทุกไลฟ์สไตล์!
+        </p>
+      </div>
+      {/* ------------------------------------------------------------------------------------------------------------------------------- */}
     </section>
   );
 }
--- src/Allpage/Page7.jsx
+++ src/Allpage/Page7.jsx
@@ -4,8 +4,32 @@ import "../App.css";
 export default function Contact() {
   const [count, setCount] = useState(0);
   return (
-  <section className="w-screen h-screen bg-blue-100" id="contact">
-    
-  </section>
+    <section className="w-screen h-screen max-h-screen  bg-white flex flex-col " id="contact">
+      
+      <div className="w-full h-[9rem] items-center flex-col flex place-content-center ">
+        <h1 className="Thai text-4xl text-center mb-3">ช่องทางติดต่อ</h1>
+        <div className="w-[15%] self-center bg-black h-[0.35rem] rounded-full mb-0"></div>
+      </div>
+
+      <div className="w-full h-[15rem] text-left text-4xl Thai-light flex flex-col content-center justify-center items-center">
+        <p className="w-[80%]">📞 <span className="Thai-EX text-[#24149B]">ติดต่อเรา :</span> 089-788-0964</p>
+        <p className="w-[80%]">📍 <span className="Thai-EX text-[#24149B]">ที่ตั้ง :</span>  253/969 ถ.เคหะร่มเกล้า64 เขต ลาดกระบัง แขวง คลองสองต้นนุ่น</p>
+        <p className="w-[80%]">📌 <span className="Thai-EX text-[#24149B]">Facebook :</span> Gina English Center</p>
+      </div>
+      <div className="w-full h-[10rem] bg-[#24149B] Thai-MD text-white justify-center  flex items-center ">
+        <h1 className="text-5xl text-center">✨ พิสูจน์ด้วยตัวคุณเองว่า "ภาษาอังกฤษไม่ยากอย่างที่คิด!" ✨</h1>
+      </div>
+
+      <div className="map-container w-full h-[40%] max-h-[50rem]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.1049017572504!2d100.72226557587287!3d13.772546996811325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d65f2209f4c95%3A0x1cfe8836d4de0e56!2sTechademics!5e0!3m2!1sth!2sth!4v1732886393651!5m2!1sth!2sth"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full h-full aspect-video border-0"
+        ></iframe>
+      </div>
+    </section>
   );
 }
