5f84d1e2b8b117b8c20d99d4d0eb9e67e771057d
Oshirigami <104879528+BANK12384@users.noreply.github.com>
2025-06-19 16:03:08 +0700

    fix bug

--- src/Allpage/Page7.jsx
+++ src/Allpage/Page7.jsx
@@ -39,7 +39,7 @@ export default function Contact() {
             ✨ พิสูจน์ด้วยตัวคุณเองว่า "ภาษาอังกฤษไม่ยากอย่างที่คิด!" ✨
           </h1>
         </div>
-        <div className="map-container w-full lg:h-[50%] xl:h-[40%] max-h-[50rem]">
+        <div className="map-container w-full lg:h-[50%] xl:h-[40%] 2xl:h-[50%] max-h-[50rem]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.104462609181!2d100.7222763751369!3d13.772573486621463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d65474153fd97%3A0x57b769058e3cfc1e!2sGina%20English%20Center!5e0!3m2!1sth!2sth!4v1749451537908!5m2!1sth!2sth"
             style={{ border: 0 }}
