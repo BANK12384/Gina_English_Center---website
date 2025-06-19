96302520baa3ac9580388bc219877831d137dcc1
Oshirigami <104879528+BANK12384@users.noreply.github.com>
2025-06-19 15:05:39 +0700

    fix studen info 1

--- src/Allpage/StudentInfo2.jsx
+++ src/Allpage/StudentInfo2.jsx
@@ -32,10 +32,10 @@ function SlideSP({ items, imageUrl }) {
             className="mb-6 flex flex-col content-center justify-center"
           >
             <h1 className="Thai text-center text-5xl mt-4">{item.title}</h1>
-            <div className={`h-[0.35rem] rounded-full my-2 mb-2 bg-[#313131] self-center ${item.widthLong}`}/>
-            <div className="w-[60%] h-[25rem] bg-black self-center">
-
-            </div>
+            <div
+              className={`h-[0.35rem] rounded-full my-2 mb-2 bg-[#313131] self-center ${item.widthLong}`}
+            />
+            <div className="w-[60%] h-[25rem] bg-black self-center"></div>
           </div>
         ))}
       </div>
@@ -56,50 +56,52 @@ export default function Studentinfo2() {
       className="w-screen h-screen flex flex-col content-center z-20 text-center justify-center justify-items-center"
       id="Studentinfo2"
     >
-      {/* background layers */}
-      <div className="absolute inset-0 bg-[url('/img/StudentInifo.svg')] bg-center bg-cover bg-fixed h-full z-10"></div>
-      <div className="absolute inset-0 bg-black bg-opacity-55 z-10"></div>
+      <div className="w-full h-full lg:flex lg:flex-col hidden">
+        {/* background layers */}
+        <div className="absolute inset-0 bg-[url('/img/StudentInifo.svg')] bg-center bg-cover bg-fixed h-full z-10"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-55 z-10"></div>
 
-      {/* header */}
-      <h1 className="z-20 text-white Eng text-8xl font-bold my-2">
-        Student info
-      </h1>
-      <div className="px-80 h-1 bg-white z-20 self-center rounded-full mb-10"></div>
+        {/* header */}
+        <h1 className="z-20 text-white Eng text-8xl font-bold my-2">
+          Student info
+        </h1>
+        <div className="px-80 h-1 bg-white z-20 self-center rounded-full mb-10"></div>
 
-      {/* content card */}
-      <div className="bg-[#F2CF67] w-full h-[65%] z-20 input-wrapper flex shadow-lg">
-        {/* left profile box */}
-        <div className="w-[45rem] flex-none flex flex-col items-center">
-          <div className="bg-black border-[0.4rem] w-[60%] h-[60%] self-center rounded-lg mt-[3.4rem] bg-cover bg-[url('/img/Dan103.png')] bg-center" />
-          <div className="w-[30%] h-[3rem] bg-black mt-5 flex justify-center text-white rounded-full">
-            <p className="text-center self-center">NAME</p>
+        {/* content card */}
+        <div className="bg-[#F2CF67] w-full h-[65%] z-20 input-wrapper flex shadow-lg">
+          {/* left profile box */}
+          <div className="w-[45rem] flex-none flex flex-col items-center">
+            <div className="bg-black border-[0.4rem] w-[60%] h-[60%] self-center rounded-lg mt-[3.4rem] bg-cover bg-[url('/img/Dan103.png')] bg-center" />
+            <div className="w-[30%] h-[3rem] bg-black mt-5 flex justify-center text-white rounded-full">
+              <p className="text-center self-center">NAME</p>
+            </div>
+            <div className="grid grid-cols-2 text-2xl">
+              <p className="p-3">AGE : None</p>
+              <p className="p-3">Gender : M</p>
+            </div>
           </div>
-          <div className="grid grid-cols-2 text-2xl">
-            <p className="p-3">AGE : None</p>
-            <p className="p-3">Gender : M</p>
-          </div>
-        </div>
 
-        {/* right slide section */}
-        <div className="flex-1 items-center justify-items-start content-center">
-          <div className="ml-[-4rem] w-full h-[90%] bg-[#FFE9A9] rounded-xl relative overflow-hidden">
-            <button
-              onClick={prevSlide}
-              className="absolute left-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full z-20 RotateboxLeft"
-            >
-              <img src="/Left-arrow.svg" alt="←" className="w-10" />
-            </button>
-            <button
-              onClick={nextSlide}
-              className="absolute right-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full z-20 RotateboxRight"
-            >
-              <img src="/right-arrow.svg" alt="→" className="w-10" />
-            </button>
+          {/* right slide section */}
+          <div className="flex-1 items-center justify-items-start content-center">
+            <div className="ml-[-4rem] w-full h-[90%] bg-[#FFE9A9] rounded-xl relative overflow-hidden">
+              <button
+                onClick={prevSlide}
+                className="absolute left-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full z-20 RotateboxLeft"
+              >
+                <img src="/Left-arrow.svg" alt="←" className="w-10" />
+              </button>
+              <button
+                onClick={nextSlide}
+                className="absolute right-4 top-1/2 -translate-y-1/2 bg-[#F2CF67] p-2 rounded-full z-20 RotateboxRight"
+              >
+                <img src="/right-arrow.svg" alt="→" className="w-10" />
+              </button>
 
-            <SlideSP
-              items={slides[slideIndex].items}
-              imageUrl={slides[slideIndex].imageUrl}
-            />
+              <SlideSP
+                items={slides[slideIndex].items}
+                imageUrl={slides[slideIndex].imageUrl}
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/Allpage/StudentInifo.jsx
+++ src/Allpage/StudentInifo.jsx
@@ -6,34 +6,67 @@ export default function Studentinfo() {
 
   return (
     <section
-      className="w-screen h-screen grid content-center z-20 text-center justify-center justify-items-center"
+      className="w-screen h-screen  content-center z-20 text-center justify-center justify-items-center"
       id="Studentinfo"
     >
-      <div className="absolute inset-0 bg-[url('/img/StudentInifo.svg')] bg-center bg-cover bg-fixed h-full z-10"></div>
-      <div className="absolute inset-0 bg-black bg-opacity-55 z-10"></div>
-      <h1 className="z-20 text-white Eng text-8xl font-bold my-2">
-        Student info
-      </h1>
-      <div className="px-80 h-1 bg-white z-20 self-center rounded-full mb-10"></div>
-      <div className="bg-white w-[70rem] h-[7.5rem] z-20 rounded-lg flex input-wrapper relative">
-        <label className="h-20 self-center w-full mx-7 flex items-center px-4 relative text-center">
-          <input
-            name="myInput"
-            type="text"
-            placeholder="Student Name"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            className="w-full h-full text-4xl outline-none Thai-light font-light pr-12"
-          />
-          {value && (
-            <span
-              onClick={() => setValue("")}
-              className="absolute right-3 text-6xl text-gray-400 cursor-pointer text-center mb-3 "
-            >
-              ×
-            </span>
-          )}
-        </label>
+      <div className="w-full h-full hidden lg:flex lg:flex-col ">
+        <div className="absolute inset-0 bg-[url('/img/StudentInifo.svg')] bg-center bg-cover bg-fixed h-full z-10"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-55 z-10"></div>
+        <h1 className="z-20 text-white Eng text-8xl font-bold my-2">
+          Student info
+        </h1>
+        <div className="px-80 h-1 bg-white z-20 self-center rounded-full mb-10"></div>
+        <div className="bg-white w-[70rem] h-[7.5rem] z-20 rounded-lg flex input-wrapper relative">
+          <label className="h-20 self-center w-full mx-7 flex items-center px-4 relative text-center">
+            <input
+              name="myInput"
+              type="text"
+              placeholder="Student Name"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="w-full h-full text-4xl outline-none Thai-light font-light pr-12"
+            />
+            {value && (
+              <span
+                onClick={() => setValue("")}
+                className="absolute right-3 text-6xl text-gray-400 cursor-pointer text-center mb-3 "
+              >
+                ×
+              </span>
+            )}
+          </label>
+        </div>
+      </div>
+
+      <div className="w-full h-full lg:hidden flex flex-col relative place-items-center justify-center">
+        <div className="absolute inset-0 bg-[url('/Untitleddesign(1).png')] bg-center bg-cover z-0"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-55 z-10"></div>
+
+        <div className="z-20 flex flex-col mt-[-4rem] items-center px-4 self-center">
+          <h1 className="text-white Eng text-5xl font-bold mb-2">
+            Student info
+          </h1>
+          <div className="w-[85%] h-1 bg-white rounded-full mb-5"></div>
+
+          <div className="bg-white w-[90%] h-[3rem] rounded-lg flex items-center relative px-4">
+            <input
+              name="myInput"
+              type="text"
+              placeholder="Student Name"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className="w-full h-full text-2xl outline-none Thai-light font-light pr-10"
+            />
+            {value && (
+              <span
+                onClick={() => setValue("")}
+                className="absolute right-3 text-3xl mt-[-5px] text-gray-400 cursor-pointer"
+              >
+                ×
+              </span>
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
